0837d2fe87b595cead39ddf510082b53a7fdfd5e
Maximilian Gewers <42013351+MaximilianGewers@users.noreply.github.com>
2025-10-18 06:22:48 +0000

    fix

--- resources/views/animes/show.blade.php
+++ resources/views/animes/show.blade.php
@@ -7,7 +7,6 @@
                 <x-back-link :href="route('animes.index')">
                     Back to all animes
                 </x-back-link>
-                <a href="{{ route('animes.index') }}" class="text-sm font-medium text-sky-300 transition hover:text-sky-200">← Back to all animes</a>
                 <x-section-header :title="$anime['title']" :subtitle="$anime['synopsis']" />
                 <div class="flex flex-wrap gap-2 text-xs font-semibold">
                     @foreach ($anime['genres'] as $genre)
